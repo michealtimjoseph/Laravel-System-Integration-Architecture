3c33d6d9c8803c279fce16baafffdce3a6f1da87
Micheal Tim Joseph Enriquez <michealtimjoseph.enriquez@gmail.com>
2026-03-06 14:13:22 +0800

    added controller

---
+++ app/Http/Controllers/PlantController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plant;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PlantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Plant $plant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Plant $plant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Plant $plant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Plant $plant)
+    {
+        //
+    }
+}
